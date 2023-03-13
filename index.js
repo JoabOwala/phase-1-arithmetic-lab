fbc525817c48814b6a60e7dba227c94d8605d843
Joab Owala <owalajoab@gmail.com>
2023-03-13 18:50:41 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let multiply;
+const num1 = 31;
+const num2 = 2;
+multiply = num1 * num2;
+
+let random;
+random = Math.floor(Math.random()) + 1;
+
+let mod;
+const num3 = 25;
+const num4 = 7;
+mod = num3 % num4;
+
+let max; 
+max =  Math.max(1,2,3,20);
